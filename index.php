465783c34e0f5e84512384f3bd41e739f81f6c40
Bisminla15 <86422951+Bisminla15@users.noreply.github.com>
2021-12-07 14:34:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,29 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-
 </head>
 <body>
-    <button id="btnBack"> back </button>
-    <div id="main">
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th> <th>Title</th> <th>Detail</th>
-                </tr>
-            </thead>
-            <tbody id="tblPost"> </tbody>
-        </table>
-    </div>
-    <div id="detail">
-        aaaaaaaaa
-    </div>
+<button id="btnBack"> back </button>
+<div id="main">
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th> <th>Title</th><th> Details </th>
+            </tr>
+        </thead>
+        <tbody id="tblPosts">
+        </tbody>
+    </table>
+</div>
+<div id="detail">
+    sssssss
+</div>
     
 </body>
 <script>
-    function showDetail(id){
+    function showDetails(id){
         $("#main").hide();
         $("#detail").show();
         var url = "https://jsonplaceholder.typicode.com/posts/"+id;
@@ -36,33 +35,32 @@
                 console.log(data);
             })
             .fail((xhr, status, error)=>{
-
             })
     }
-    function loadPost(){
+    function loadPosts(){
         $("#main").show();
-        $("#detail").hide;
+        $("#details").hide();
+        
         var url = "https://jsonplaceholder.typicode.com/posts";
         $.getJSON(url)
             .done((data)=>{
-                $.each(data,(k, item)=>{
+                $.each(data, (k, item)=>{
                     console.log(item);
                     var line = "<tr>";
-                        line += "<td>" + item.id + "</td>";
-                        line += "<td><b>" + item.title + "</b></br>";
-                        line +=  + item.body + "</td>";
-                        line += "<td> <button onClick='showDetail(" + item + ");'> link </Button></td>";
+                        line += "<td>"+ item.id + "</td>";
+                        line += "<td><b>"+ item.title + "</b><br/>";
+                        line += item.body + "</td>";
+                        line += "<td> <button onClick='showDetails("+ item.id +");' > link </button> </td>";
                         line += "</tr>";
-                    $("#tdlPost").append(line);
+                    $("#tblPosts").append(line);
                 });
                 $("#main").show();
             })
-            .fail((xhr, status, err)=>{
-
+            .fail((xhr, status, error)=>{
             })
     }
     $(()=>{
-        loadPost();
+        loadPosts();
         $("#btnBack").click(()=>{
             $("#main").show();
         });
